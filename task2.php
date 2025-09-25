4da6f36dd50cdbff09b6692ec06d9d65596fb8d2
Mahmoud5704 <mhnh5704@gmail.com>
2025-09-25 23:40:20 +0300

    Add files via upload

---
+++ task2.php
@@ -0,0 +1,11 @@
+<?php
+////////////////////*task 2*/////////////////////
+$a=4;
+$a++;
+echo "I have ",$a," books";
+
+echo "<br>";
+
+$a=4;
+echo "I have ",++$a," books";
+
---
+++ task3.php
@@ -0,0 +1,8 @@
+<?php
+////////////////////*task 3*////////////////////
+$sentence=null;
+$first="in ";
+$second="PHP ";
+$third="Course";
+$sentence=$first.$second.$third;
+echo $sentence;
