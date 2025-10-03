37f0f93d7fb5ca5fd159020f58cc3637857bfc8a
Mahmoud5704 <mhnh5704@gmail.com>
2025-10-03 04:26:52 +0300

    task5.php

--- task5.php
+++ task5.php
@@ -1,23 +1,15 @@
 <?php
-echo "<pre>";
-print_r($_POST);
-echo "</pre>";
-
-echo "<pre>";
-print_r($_FILES);
-echo "</pre>";
 
 if(isset($_POST['submit'])){
 $tmp=$_FILES['image']['tmp_name'];
 $image_name=$_FILES['image']['name'];
 $image_name=time()."_".$image_name;
-$upload_path = "images/basename($image_name)"; // Full path
- unlink($upload_path);
+$upload_path = "images/".basename($image_name);
 if (move_uploaded_file($tmp, $upload_path)) {
-        echo "uploaded successfully.<br>";
-        echo "<img src='$upload_path' width='200' alt='Uploaded Image'>";
-    } else {
-        echo "no uploaded file yet";
+echo "<div style='border:10px solid #2182f3; background-color:#2182f3;'>Uploaded successfully.</div><br>";
+echo "<img src='$upload_path' width='200' alt='Uploaded Image' style='border: 10px solid #2182f3; border-radius: 50%; margin: 20px auto; display: block;'>";
+ } else {
+        echo "<div style='border:10px solid red'>no uploaded file yet</div><br>";
     }
    
 }
@@ -28,26 +20,57 @@ if (move_uploaded_file($tmp, $upload_path)) {
     <head>
         <style>
             input{
-               border-color:red;
+               border-color:#2182f3;
                border-width:10px; 
                margin:20px auto;
+              
+               height: 50px;
                 display: block;
             }
-            
+          
+            button{
+                background-color:#2182f3;
+                border-radius:50%;
+                width:80px;
+                height: 50px;
+                margin:20px 200px;
+            }
+            button:hover{
+                background-color:blue;
+            }
+            form{
+                background-color:#1ab8ff;
+            }
+            div{
+                font-size:30px;
+                
+            }
         </style>
     </head>
     <body>
     <form method="POST" enctype="multipart/form-data">
-        enter your name: <input type="text" name="name" placeholder="enter name">
+       <div>enter your name: </div> <input type="text" name="name" placeholder="enter name">
         <br>
-        enter telephone: <input type="text" name="phone" placeholder="enter phone">
+       <div> enter telephone: </div><input type="text" name="phone" placeholder="enter phone">
         <br>
-        enter password : <input type="text"name="password" placeholder="enter password">
+       <div> enter password : </div><input type="text"name="password" placeholder="enter password">
         <br>
-        upload picture : <input type="file" name="image">
+       <div> upload picture : </div> <input type="file" name="image">
         <br>
 <button type="submit" name="submit">save</button>
         </form>
     </body>
 
 </html>
+
+<?php
+echo "<div style='border:10px solid #000000ff; background-color:blue;'>FOR MORE INFORMATION!</div><br>";
+
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+
+echo "<pre>";
+print_r($_FILES);
+echo "</pre>";
+?>
